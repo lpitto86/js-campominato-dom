927417b84ee68be4c739cfb2d198817715f63abe
lpitto86 <luca.p481@gmail.com>
2023-12-15 17:24:33 +0100

    Numbers

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,41 @@ function handleCellClick(cell, cellNumber) {
     // Visualizza il numero della cella nella console
     console.log(`Cella cliccata: ${cellNumber}`);
 }
+
+function generateRandomBombs(totalCells, bombCount) {
+      const bombs = [];
+      
+      while (bombs.length < bombCount) {
+          const randomBomb = Math.floor(Math.random() * totalCells) + 1;
+  
+          // Verifica se il numero è già presente nell'array delle bombe
+          if (!bombs.includes(randomBomb)) {
+              bombs.push(randomBomb);
+          }
+      }
+  
+      return bombs;
+}
+
+document.addEventListener('DOMContentLoaded', generateGame);
+
+function generateGame() {
+    const totalCells = 100;
+    const bombCount = 16;
+
+    const gridContainer = document.getElementById('gameGrid');
+    const bombs = generateRandomBombs(totalCells, bombCount);
+
+    for (let i = 1; i <= totalCells; i++) {
+        const cell = document.createElement('div');
+        cell.className = 'grid-cell';
+        cell.textContent = i;
+
+        // Aggiungi l'evento di click a ogni cella
+        cell.addEventListener('click', () => handleCellClick(cell, i, bombs));
+
+        gridContainer.appendChild(cell);
+    }
+
+    console.log('Bombe:', bombs);
+}
